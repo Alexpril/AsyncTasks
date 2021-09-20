e164bbad15b0f64422bf84ec4e4c054059d17e65
Oleksii Prylutskyi <prylutskyi@nixsolutions.com>
2021-09-20 14:58:55 +0300

    renamed properties

--- ThirdTask/ThirdTask/Program.cs
+++ ThirdTask/ThirdTask/Program.cs
@@ -45,20 +45,20 @@ namespace ThirdTask
                 key = Console.ReadKey(true).Key;
                 if (key == ConsoleKey.D1 || key == ConsoleKey.NumPad1)
                 {
-                    var productGroups = from product in products group product by product.category;
+                    var productGroups = from product in products group product by product.Category;
 
                     foreach (IGrouping<Categories, Product> group in productGroups)
                     {
                         Console.Write("\n\n\n" + group.Key + "\n\n\n");
                         foreach (var groupProduct in group)
-                            Console.Write("\n Name: {0}\n Category: {1}\n Price: {2}\n Quantity: {3}\n", groupProduct.name, groupProduct.category, groupProduct.price, groupProduct.quantity);
+                            Console.Write("\n Name: {0}\n Category: {1}\n Price: {2}\n Quantity: {3}\n", groupProduct.Name, groupProduct.Category, groupProduct.Price, groupProduct.Quantity);
                     }
                 } else if (key == ConsoleKey.D2 || key == ConsoleKey.NumPad2)
                 {
                     int quantity = 0;
                     foreach(Product product in products)
                     {
-                        quantity += product.quantity;
+                        quantity += product.Quantity;
                     }
                     Console.WriteLine("Total Quantity: {0}\n", quantity);
                 } else if(key == ConsoleKey.D3 || key == ConsoleKey.NumPad3)
@@ -66,16 +66,16 @@ namespace ThirdTask
                     double sum = 0;
                     foreach (Product product in products)
                     {
-                        sum += product.price * product.quantity;
+                        sum += product.Price * product.Quantity;
                     }
                     Console.WriteLine("Total Price: {0}\n", sum);
                 } else if(key == ConsoleKey.D4 || key == ConsoleKey.NumPad4)
                 {
-                    var sortprod = from product in products orderby product.name select product;
+                    var sortprod = from product in products orderby product.Name select product;
 
                     foreach (Product prod in sortprod)
                     {
-                        Console.Write("\n Name: {0}\n Category: {1}\n Price: {2}\n Quantity: {3}\n", prod.name, prod.category, prod.price, prod.quantity);
+                        Console.Write("\n Name: {0}\n Category: {1}\n Price: {2}\n Quantity: {3}\n", prod.Name, prod.Category, prod.Price, prod.Quantity);
                     }
                     Console.WriteLine("Sort by name\n");
                 }
